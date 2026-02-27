c82c3c216305bb47373320752d1ace4ec288d963
Sander077 <Sandvans3@student.arteveldehs.be>
2026-02-27 12:58:30 +0100

    server geupdate

---
+++ eindproject/server/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express";
+import path from "path";
+
+const app: Application = express();
+const PORT: number = 3000;
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req: Request, res: Response): void => {
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server draait op http://localhost:${PORT}`);
+});
